f78e4150c0a44117d07058eac274612d70db88fd
sparku7 <sparku7@sky.com>
2024-08-06 14:41:15 +0100

    Updated shop page to pull from db

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,37 +1,36 @@
-import itemsData from "../itemsData.json";
+import axios from 'axios';
 import ItemCard from '../components/ItemCard';
 import { useEffect, useState } from "react";
 
-
 const ShopPage = () => {
 
-    const apiURL = 'https://seed-theory-api.netlify.app/data.json'
-    const [items, setItems] = useState(['']);
-
+    const apiURL = 'http://localhost:8081/items/getAll';
+    const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch(apiURL)
-            .then((response) => response.json())
-            .then((data) => setItems(data))
+        axios.get(apiURL)
+            .then((response) => {
+                console.log('Data fetched from API:', response.data); 
+                setItems(response.data);
+            })
+            .catch((error) => console.error('Error fetching items:', error));
+    }, []);
 
-    }, [])
-
-
-    return(
+    return (
         <div className="body">
             <h1 className="shopHeader">Our Stationery</h1>
             <div className="shopItems">
                 {items.map((item) => (
-                        <ItemCard
-                        id = {item.id}
-                        name = {item.name}
-                        price = {item.price}
-                        imageUrl = {item.imageUrl}
-                        />
+                    <ItemCard                   
+                        id={item.id}
+                        name={item.name}
+                        price={item.price}
+                        imageUrl={`http://example.com/images/${item.id}.jpg`} 
+                    />
                 ))}
             </div>
         </div>
-    )
+    );
 };
 
 export default ShopPage;
