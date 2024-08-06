c5d11e1a5d8f3d08d83e2756e34b0b9a67be1f21
dmc93 <mcmanus-93@hotmail.co.uk>
2024-08-06 11:55:47 +0100

    Updated confetti

--- src/components/eventcomponent/AddConfetti.jsx
+++ src/components/eventcomponent/AddConfetti.jsx
@@ -2,7 +2,7 @@ import JSConfetti from 'js-confetti'
 
 const jsConfetti = new JSConfetti()
 const addConfetti = () => {
-    jsConfetti.addConfetti({ emojis: ['🎈', '🥳', '🎉'], confettiNumber: 32  }) 
+    jsConfetti.addConfetti({ emojis: ['🐪', '🌴', '🌵'], confettiNumber: 32  }) 
 }
 
 export default addConfetti;
