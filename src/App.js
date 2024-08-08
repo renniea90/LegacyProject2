c83da06fb9e332b4dfdd64599d21c4b67048fff8
sparku7 <sparku7@sky.com>
2024-08-08 10:24:41 +0100

    Tidied up navbar CSS and made bar fixed.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import './App.css';
-
+import './CSS/App.css';
 import Navbar from './pages/Navbar';
 import HomePage from './pages/HomePage';
 import CartPage from './pages/CartPage';
 import ShopPage from './pages/ShopPage';
+import Admin from './pages/Admin';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './components/CartProvider';
-import Admin from './pages/Admin';
-
-
-
 
 function App() {
   return (
     <div className="App">
-      <div className="title-banner">
-        <h1 className='site-title'>Sahara Stationery</h1>
-      </div>
       <BrowserRouter>
         <Navbar />
         <CartProvider>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/shop' element={<ShopPage />} />
-          <Route path='/cart' element={<CartPage />} />
-          <Route path='/admin' element={<Admin />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
         </CartProvider>
-      </BrowserRouter>   
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,22 +1,28 @@
 import { Link } from 'react-router-dom'
+import Navbarcss from '../CSS/Navbar.css'
 
-export default function Navbar() {
-    return(
-        <nav>
-            <ul className='nav'>
-                <li className='nav-item'>
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-header">
+                <h1 className="site-title">Sahara Stationery</h1>
+            </div>
+            <ul className="nav">
+                <li className="nav-item">
                     <Link to="/">Home</Link>
                 </li>
-                <li className='nav-item'>
+                <li className="nav-item">
                     <Link to="/shop">Shop</Link>
                 </li>
-                <li className='nav-item'>
+                <li className="nav-item">
                     <Link to="/cart">Cart</Link>
                 </li>
-                <li className='nav-item'>
+                <li className="nav-item">
                     <Link to="/admin">Admin</Link>
                 </li>
             </ul>
         </nav>
-    )
-}
+    );
+};
+
+export default Navbar;
