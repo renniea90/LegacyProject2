09b090026883a9c97c81ba6ec462436ef35bd1f4
sparku7 <sparku7@sky.com>
2024-08-09 14:29:10 +0100

    Updated so that no quantity is shown if out of stock

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,5 +1,5 @@
-import { useState, useCallback } from "react";
-import CartButton from "./CartButton";
+import React, { useState, useCallback } from 'react';
+import CartButton from './CartButton';
 
 function ItemCard({ id, name, price, imageUrl, quantity }) {
   const [inputQuantity, setInputQuantity] = useState(1);
@@ -13,13 +13,14 @@ function ItemCard({ id, name, price, imageUrl, quantity }) {
     <div className="card">
       <h2>{name}</h2>
       <h3>£{price?.toFixed(2)}</h3>
-      <img className="card-image" src={imageUrl} alt={name} height={"50px"} />
+      <img className="card-image" src={imageUrl} alt={name} height="50px" />
       <br />
-      <p className="stock-info">Amount Available: {quantity}</p>
+      {quantity > 0 && (
+        <p className="stock-info">Amount Available: {quantity}</p>
+      )}
       {quantity > 0 ? (
         <div className="quantityContainer">
           <label htmlFor={`quantity-${id}`} className="quantityLabel">Qty:</label>
-          
           <select
             id={`quantity-${id}`}
             value={inputQuantity}
@@ -30,7 +31,6 @@ function ItemCard({ id, name, price, imageUrl, quantity }) {
               <option key={num} value={num}>{num}</option>
             ))}
           </select>
-          
           <br />
           <CartButton />
         </div>
