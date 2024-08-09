92b729587d0adb4c8b99572cfdab4c74014f57b1
sparku7 <sparku7@sky.com>
2024-08-09 12:57:58 +0100

    Added burger menu

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,13 +1,25 @@
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../CSS/Navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-header">
                 <h1 className="site-title">Sahara Stationery</h1>
+                <div className="menu-toggle" onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
             </div>
-            <ul className="nav">
+            <ul className={`nav ${isOpen ? 'active' : ''}`}>
                 <li className="nav-item">
                     <Link to="/">Home</Link>
                 </li>
