42ebb112965440d3fd747fd72db4d4a6014fae78
sparku7 <sparku7@sky.com>
2024-08-09 12:38:58 +0100

    added images to product lists.

--- src/components/ProductListTable.jsx
+++ src/components/ProductListTable.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-
 const ProductListTable = ({ products, onUpdate, onDelete, onRequestSort, sortConfig }) => {
+
     const sortedProducts = [...products].sort((a, b) => {
         if (a[sortConfig.key] < b[sortConfig.key]) {
             return sortConfig.direction === 'ascending' ? -1 : 1;
@@ -21,6 +21,7 @@ const ProductListTable = ({ products, onUpdate, onDelete, onRequestSort, sortCon
                         <th onClick={() => onRequestSort('name')}>Product Name</th>
                         <th onClick={() => onRequestSort('price')}>Price</th>
                         <th onClick={() => onRequestSort('quantity')}>Quantity</th>
+                        <th>Image</th>
                         <th>Update Product</th>
                         <th>Delete Product</th>
                     </tr>
@@ -32,6 +33,13 @@ const ProductListTable = ({ products, onUpdate, onDelete, onRequestSort, sortCon
                             <td>{product.name}</td>
                             <td>£{product.price.toFixed(2)}</td>
                             <td>{product.quantity}</td>
+                            <td>
+                              
+                                <img
+                                    src={product.imageUrl}
+                                    alt={product.name}                                
+                                />
+                            </td>
                             <td>
                                 <button className="update-btn" onClick={() => onUpdate(product)}>Update</button>
                             </td>
