7f128c3820b75c43a30f07eef779d42ae284e383
sparku7 <sparku7@sky.com>
2024-08-09 15:58:48 +0100

    Amended navbar css

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -11,28 +11,29 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
-            <div className="navbar-header">
+            <div className="navbar-content">
                 <h1 className="site-title">Sahara Stationery</h1>
                 <div className="menu-toggle" onClick={toggleMenu}>
                     <span className="bar"></span>
                     <span className="bar"></span>
                     <span className="bar"></span>
+                    <span>Menu</span>
                 </div>
+                <ul className={`nav ${isOpen ? 'active' : ''}`}>
+                    <li className="nav-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/shop">Shop</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/cart">Cart</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/admin">Admin</Link>
+                    </li>
+                </ul>
             </div>
-            <ul className={`nav ${isOpen ? 'active' : ''}`}>
-                <li className="nav-item">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/shop">Shop</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/cart">Cart</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/admin">Admin</Link>
-                </li>
-            </ul>
         </nav>
     );
 };
